d93700e8f3f46089037c5bb86341350f7c7d5fc7
romainlmrtl <romain.lemartinel@sts-sio-caen.info>
2021-12-22 00:15:19 +0100

    actuelle

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width"/>
     <link rel="stylesheet" href="style.css">
-    <title>Présentation Professionnel</title>
+    <title>Présentation Professionnelle</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css"/>
     <script src="https://www.google.com/recaptcha/api.js?render=ICI_LA_CLE_DU_SITE"></script>
+    <link rel="icon" type="image/png" href="images/favicon.png" />
 </head>
 <body>
 <div class="contenu">
